5ac76b63e5a86fde500236a5490b7d414ab98695
keiakihito <kkatsumi.git@gmail.com>
2025-10-15 06:23:40 -0700

    [Add] Add BurgerTest.java for a redbar test.

--- decorator/src/test/java/restaurant/fooditems/BurgerTest.java
+++ decorator/src/test/java/restaurant/fooditems/BurgerTest.java
@@ -8,4 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class BurgerTest {
 
+    @Test
+    public void testBurgerCost() {
+        Burger burger = new Burger();
+        assertEquals(5.00, burger.getCost(), 0.01);
+    }
+
+    @Test
+    public void testBurgerDescription() {
+        Burger burger = new Burger();
+        assertEquals("Burger", burger.getDescription());
+    }
 }
