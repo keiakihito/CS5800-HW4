e7e6de412d10a6a9abee4305b3935ea7c9b4a092
keiakihito <kkatsumi.git@gmail.com>
2025-10-15 06:42:22 -0700

    [Add] HotDotTest.java for a redbar test.

--- decorator/src/main/java/restaurant/components/HotDog.java
+++ decorator/src/main/java/restaurant/components/HotDog.java
@@ -1,8 +1,16 @@
-// package restaurant.components;
+package restaurant.components;
 
-// /**
-//  * Concrete food item: Hot Dog with base price.
-//  */
-// public class HotDog implements FoodItem {
+/**
+ * Concrete food item: Hot Dog with base price.
+ */
+public class HotDog implements FoodItem {
+    @Override
+    public double getCost() {
+        return 0.00;
+    }
 
-// }
+    @Override
+    public String getDescription() {
+        return "";
+    }
+}
--- decorator/src/test/java/restaurant/fooditems/HotDogTest.java
+++ decorator/src/test/java/restaurant/fooditems/HotDogTest.java
@@ -3,9 +3,22 @@ package restaurant.fooditems;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import restaurant.components.HotDog;
+
 /**
  * Unit tests for HotDog class.
  */
 public class HotDogTest {
+    @Test
+    public void testHotDogCost() {
+        HotDog hotDog = new HotDog();
+        assertEquals(2.00, hotDog.getCost(), 0.01); // 0.01 is the margin of error
+    }
+    
+    @Test
+    public void testHotDogDescription() {
+        HotDog hotDog = new HotDog();
+        assertEquals("HotDog", hotDog.getDescription());
+    }
 
 }
