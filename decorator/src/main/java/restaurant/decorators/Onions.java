2e9f1e814f22f29f1f87084ea86c79b873804a52
keiakihito <kkatsumi.git@gmail.com>
2025-10-15 07:15:33 -0700

    [Add] OnionTest.java for a redbar test.

--- decorator/src/main/java/restaurant/decorators/Onions.java
+++ decorator/src/main/java/restaurant/decorators/Onions.java
@@ -1,8 +1,20 @@
-// package restaurant.decorators;
+package restaurant.decorators;
 
-// /**
-//  * Concrete decorator: Adds onions topping to a food item.
-//  */
-// public class Onions extends ToppingDecorator {
+import restaurant.components.FoodItem;
 
-// }
+/**
+ * Concrete decorator: Adds onions topping to a food item.
+ */
+public class Onions extends ToppingDecorator {
+    public Onions(FoodItem foodItem) {
+        super(foodItem);
+    }
+    @Override
+    public double getCost() {
+        return 0.00;
+    }
+    @Override
+    public String getDescription() {
+        return "";
+    }
+}
--- decorator/src/test/java/restaurant/decorators/OnionsTest.java
+++ decorator/src/test/java/restaurant/decorators/OnionsTest.java
@@ -3,9 +3,41 @@ package restaurant.decorators;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import restaurant.components.Burger;
+import restaurant.components.HotDog;
+import restaurant.decorators.Onions;
+
+
 /**
  * Unit tests for Onions topping decorator.
  */
 public class OnionsTest {
+    @Test
+    public void testOnionsAddsToBurgerCost() {
+        Burger burger = new Burger();
+        Onions onionsBurger = new Onions(burger);
+        assertEquals(7.25, onionsBurger.getCost(), 0.01);
+    }
+    @Test
+    public void testOnionsAddsToDescription() {
+        Burger burger = new Burger();
+        Onions onionsBurger = new Onions(burger);
+        assertEquals("Burger + Onions", onionsBurger.getDescription());
+    }
+    @Test
+    public void testOnionsOnHotDog() {
+        HotDog hotDog = new HotDog();
+        Onions onionsHotDog = new Onions(hotDog);
+        assertEquals(2.25, onionsHotDog.getCost(), 0.01);
+        assertEquals("HotDog + Onions", onionsHotDog.getDescription());
+    }
+    @Test
+    public void testMultipleOnions() {
+        Burger burger = new Burger();
+        Onions onionsBurger = new Onions(burger);
+        Onions doubleOnions = new Onions(onionsBurger);
+        assertEquals(7.50, doubleOnions.getCost(), 0.01);
+        assertEquals("Burger + Onions + Onions", doubleOnions.getDescription());
+    }
 
 }
