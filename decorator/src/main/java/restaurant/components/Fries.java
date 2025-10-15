ffa5c5e648b5e2a8f78aca0afc21598e1ff196ff
keiakihito <kkatsumi.git@gmail.com>
2025-10-15 06:40:40 -0700

    [Add] Fries.java and passed the unit test.

--- decorator/src/main/java/restaurant/components/Fries.java
+++ decorator/src/main/java/restaurant/components/Fries.java
@@ -4,13 +4,13 @@ package restaurant.components;
  * Concreate Component for an item, Fries.
  */
 public class Fries implements FoodItem {
-    // @Override
-    // public double getCost() {
-    //     return 5.00;
-    // }
+    @Override
+    public double getCost() {
+        return 5.00;
+    }
 
-    // @Override
-    // public String getDescription() {
-    //     return "Fries";
-    // }
+    @Override
+    public String getDescription() {
+        return "Fries";
+    }
 }
